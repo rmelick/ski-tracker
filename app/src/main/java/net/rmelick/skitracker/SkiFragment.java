1d81ba6821fd89cb6707c2e27c8b5a0e8bc727a4
Russell Melick <rmelick@linkedin.com>
2015-01-30 10:38:09 -0800

    Initial commit

---
+++ app/src/main/java/net/rmelick/skitracker/SkiFragment.java
@@ -0,0 +1,28 @@
+package net.rmelick.skitracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by rmelick on 1/30/15.
+ */
+public class SkiFragment {
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_ski, container, false);
+            return rootView;
+        }
+    }
+}
