353fbb5d077ceb6511ff779ec038d717cc2ec4b3
Russell Melick <rmelick@linkedin.com>
2015-01-30 16:30:22 -0800

    Add options menu to create new ski daysx

--- app/src/main/java/net/rmelick/skitracker/ui/SkiDayListFragment.java
+++ app/src/main/java/net/rmelick/skitracker/ui/SkiDayListFragment.java
@@ -1,10 +1,14 @@
 package net.rmelick.skitracker.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
@@ -20,11 +24,13 @@ import net.rmelick.skitracker.SkiDayManager;
  * @author rmelick
  */
 public class SkiDayListFragment extends ListFragment {
+  private static final int REQUEST_NEW_SKI_DAY = 0;
   private SkiDatabaseHelper.SkiDayCursor mCursor;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
     // query list of saved ski days
     mCursor = SkiDayManager.get(getActivity()).querySkiDays();
     // adapter points at the cursor
@@ -36,6 +42,33 @@ public class SkiDayListFragment extends ListFragment {
   public void onDestroy() {
     mCursor.close();
     super.onDestroy();
+  }
+
+  /** We don't need this since our ListActivity inflates the menu already
+  @Override
+  public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    super.onCreateOptionsMenu(menu, inflater);
+    inflater.inflate(R.menu.menu_ski_day_list, menu);
+  }**/
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.menu_item_new_ski_day:
+        Intent i = new Intent(getActivity(), SkiActivity.class);
+        startActivityForResult(i, REQUEST_NEW_SKI_DAY);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (REQUEST_NEW_SKI_DAY == requestCode) {
+      mCursor.requery();
+      ((SkiDayCursorAdapter)getListAdapter()).notifyDataSetChanged();
+    }
   }
 
   private static class SkiDayCursorAdapter extends CursorAdapter {
